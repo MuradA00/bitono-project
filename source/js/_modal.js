7b25ae4ea8563e36a9e2c1be6abbae89434fd0a7
Murad <abishlimurad@gmail.com>
2023-02-23 12:13:43 +0200

    table and withdrawModal added

--- source/js/_modal.js
+++ source/js/_modal.js
@@ -1,7 +1,9 @@
 const transferBtns = document.querySelectorAll('.dashboard__buy'),
       transferModal = document.querySelector('#transferModal'),
       closeIconModal = document.querySelector('#transferModal .modal__close'),
-      closeTransferModal = document.querySelector('#transferModal .modal__close-btn');
+      closeTransferModal = document.querySelector('#transferModal .modal__close-btn'),
+      withModalTrigger = document.querySelector('#withModalTrigger'),
+      withModal = document.querySelector('#withModal');
 
 // function openModal(modal) {
 //   modal.classList.add('show-modal');
@@ -15,8 +17,12 @@ function closeModal(closeIcon, closeBtn, modal) {
   closeBtn.addEventListener('click', () => {
     modal.classList.remove('show-modal'), document.body.style.overflow = ''
   })
-
 }
+
+withModalTrigger.addEventListener('click', () => {
+  withModal.classList.add('show-modal')
+  document.body.style.overflow = 'hidden'
+})
 
 transferBtns.forEach(btn => {
   btn.addEventListener('click', () => {
@@ -24,6 +30,14 @@ transferBtns.forEach(btn => {
     document.body.style.overflow = 'hidden'
   })
 })
+
+
+
+closeModal(
+  document.querySelector('#withModal .modal__close'),
+  document.querySelector('#withModal .modal__close-btn'),
+  withModal
+);
 
 closeModal(closeIconModal, closeTransferModal, transferModal);
 
